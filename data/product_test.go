e6a9987beea10de0f25c88eeb057591230598a69
Jayce <nguyenxuanmanhhung@gmail.com>
2024-12-03 11:13:11 +0700

    Done ep 6

--- data/product_test.go
+++ data/product_test.go
@@ -1,1 +1,17 @@
 package data
+
+import "testing"
+
+func TestChecksValidation(t *testing.T) {
+	p := &Product{
+		Name:  "Jayce",
+		Price: 1.00,
+		SKU:   "abs-abc-def",
+	}
+
+	err := p.Validate()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
--- handlers/products.go
+++ handlers/products.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 	"github.com/gorilla/mux"
 	"jayce/data"
 	"log"
@@ -73,6 +74,18 @@ func (p Products) MiddlewareValidateProduct(next http.Handler) http.Handler {
 		if err != nil {
 			p.l.Println("[ERROR] deserializing product", err)
 			http.Error(rw, "Error reading product", http.StatusBadRequest)
+			return
+		}
+
+		//validate the product
+		err = prod.Validate()
+		if err != nil {
+			p.l.Println("[ERROR] validating product", err)
+			http.Error(
+				rw,
+				fmt.Sprintf("Error validating product: %s", err),
+				http.StatusBadRequest,
+			)
 			return
 		}
 
