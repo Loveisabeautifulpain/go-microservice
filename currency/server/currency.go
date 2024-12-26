f0ac782d5a2c7a5baa78d2796a8adcf73a97c497
Jayce <nguyenxuanmanhhung@gmail.com>
2024-12-26 21:15:10 +0700

    Done ep 13

--- currency/server/currency.go
+++ currency/server/currency.go
@@ -1,1 +1,21 @@
 package server
+
+import (
+	"context"
+	protos "currency/protos/currency/protos"
+	"github.com/hashicorp/go-hclog"
+)
+
+type Currency struct {
+	protos.UnimplementedCurrencyServer // Embed the unimplemented server
+	log                                hclog.Logger
+}
+
+func (c Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos.RateResponse, error) {
+	c.log.Info("Handle request for GetRate", "base", rr.GetBase(), "dest", rr.GetDestination())
+	return &protos.RateResponse{Rate: 0.5}, nil
+}
+
+func NewCurrency(log hclog.Logger) *Currency {
+	return &Currency{log: log}
+}
